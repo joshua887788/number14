6bfb578e856ea8f296e885fbb20adf5ecf78b9ac
joshua <joshuacrawford8877@gmail.com>
2023-09-19 17:25:28 -0400

    1

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const exphbs = require('express-handlebars');
 const app = express();
-app.engine('handlebars', exphbs());
+app.engine('handlebars', exphbs.engine);
 app.set('view engine', 'handlebars');
 
 
